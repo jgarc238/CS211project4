c68cf6a5cd6b2d2c7461adbade328f6a807f80d3
jgarc238 <78009862+jgarc238@users.noreply.github.com>
2021-10-18 11:50:36 -0500

    1st 3 functions

--- bpgame.c
+++ bpgame.c
@@ -9,27 +9,18 @@
 #include <string.h>
 #include "bpgame.h"
 
-//#include MAX_ROWS 40
-//#include MAX_COLS 40
-
-
-/** #include statements... **/
-
-
 
 /** TYPEDEFS / STRUCTs HERE ***/
 
 
 struct bpgame {
 
-
-   // YOU DECIDE WHAT TO PUT IN HERE TO CAPTURE THE STATE OF
-   //   A "RUN" OF THE GAME.....
-
    char **m;// matrix that contains all the values
-   int size;// size of the matrix
    int top;// used for push and pop of values
    int score;//holds the score for the user
+   int row;//stores the number of rows
+   int col;//stores the number of columns
+
 };
 
 
@@ -37,18 +28,85 @@ BPGame * bp_create(int nrows, int ncols){
    
    BPGame * bp = malloc(sizeof(struct bpgame));
    
+   if(nrows > MAX_ROWS || ncols > MAX_COLS){
+      fprintf(stderr, "nrows or ncols is larger than 40");
+      return NULL;
+   }
+
    char** m1;
-   m1 = (char**) malloc (sizeof(char*) * ncols);
-   for(int i = 0; i < ncols; i++){
-      m1[i] = (char*) malloc (sizeof(char) * nrows);
+   bp->row = nrows;
+   bp->col = ncols;
+   m1 = (char**) malloc (sizeof(char*) * nrows);
+
+   for(int i = 0; i < nrows; i++){
+      m1[i] = (char*) malloc (sizeof(char) * ncols);
    }
 
    bp->m = m1;
+   free(m1);
+
+   srand(time(0));
+
+   for(int i = 0; i < nrows; i++){
+      for(int j = 0; j < ncols; j++){
+         bp->m[i][j] = "^=o+"[rand () % 4]; 
+      }
+   }
+
    bp->top = -1;
    bp->score = 0;
 
+   return bp;
+}
+
+BPGame * bp_create_from_mtx(char mtx[][MAX_COLS], int nrows, int ncols){
+
+   BPGame * bp = malloc(sizeof(struct bpgame));
+
+   if(nrows > MAX_ROWS || ncols > MAX_COLS){
+      fprintf(stderr, "nrows or ncols is larger than 40");
+      return NULL;
+   }
+
+   char** m1;
+   bp->row = nrows;
+   bp->col = ncols;
+   m1 = (char**) malloc (sizeof(char*) * nrows);
+
+   for(int i = 0; i < nrows; i++){
+      m1[i] = (char*) malloc (sizeof(char) * ncols);
+   }
+
+   bp->m = m1;
+   free(m1);
+
+   for(int i = 0; i < nrows; i++){
+      for(int j = 0; j < ncols; j++){
+         if(mtx[i][j] != '^' || mtx[i][j] != '=' || mtx[i][j] != 'o' || mtx[i][j] != '+'){
+            fprintf(stderr, "One of the values in the matrix given is not a color");
+            return NULL;
+         } 
+         else{
+            bp->m[i][j] = mtx[i][j];
+         }
+      }
+   }
+
+   return bp;
+}
+
+void bp_destroy(BPGame * b){
+
+   for(int i = 0; i < b->row; i++){
+      free(b->m[i]);
+   }
+
+   free(b->m);
+}
+
+void bp_display(BPGame * b){
+   
 }
 
 
-/*** IMPLEMENTATION OF bp_XXXX FUNCTIONS HERE  ****/
 
